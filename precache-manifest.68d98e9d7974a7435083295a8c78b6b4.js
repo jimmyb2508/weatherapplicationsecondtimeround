f8dc6b40a77b4c88b717653eabc46af13747e81c
jimmyb2508 <james.barrington1985@gmail.com>
2020-01-11 11:37:47 +0000

    Updates

---
+++ precache-manifest.68d98e9d7974a7435083295a8c78b6b4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b64bc0a9314e9643bd69bfb5b5ff104",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0107161369fb22528ea9",
+    "url": "/static/css/2.a64ed4af.chunk.css"
+  },
+  {
+    "revision": "2e96a252878ae3a0b97a",
+    "url": "/static/css/main.8ee353af.chunk.css"
+  },
+  {
+    "revision": "0107161369fb22528ea9",
+    "url": "/static/js/2.c5b84109.chunk.js"
+  },
+  {
+    "revision": "2e96a252878ae3a0b97a",
+    "url": "/static/js/main.7f4a7199.chunk.js"
+  },
+  {
+    "revision": "a7205a87eb7e575457d0",
+    "url": "/static/js/runtime-main.7baed2a6.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
